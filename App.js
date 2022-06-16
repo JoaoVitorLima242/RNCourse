03d653ed000e0c33fc5e4666acd522cab4469aa6
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 14:13:17 -0300

    Studing components

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text>Teste dois</Text>
+        <TextInput placeholder='Your course goal!'/>
+        <Button title='Add goals'/>
       </View>
-      <Text style={styles.dummyText}>Teste</Text>
-      <Button title='tap new!!!'/>
-      <StatusBar style="auto" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  dummyText: {
-    padding: 10,
-    borderWidth: 1, 
-    borderColor: 'red',
-    margin: 10
+  appContainer: {
+    padding: 50,
   }
 });
