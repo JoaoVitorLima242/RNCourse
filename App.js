5348d345ccedf35ad5ad60d44cb0aa1592889b20
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 14:41:50 -0300

    Using useState in native

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    console.log(enteredGoalText)
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='Your course goal!'/>
-        <Button title='Add goals'/>
+        <TextInput 
+          style={styles.TextInput} 
+          placeholder='Your course goal!' 
+          onChangeText={goalInputHandler}
+        />
+        <Button title='Add goals' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
