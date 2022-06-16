8ab305e60e5151290445bb25db7780703e0e72c7
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 14:31:19 -0300

    add more styles

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput style={styles.TextInput} placeholder='Your course goal!'/>
         <Button title='Add goals'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -17,18 +17,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
 
   TextInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
+  },
+
+  goalsContainer: {
+    flex: 4
   }
 });
