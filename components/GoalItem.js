ecc134174a02e9488c5fbdbca355a2716ed04cb5
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 20:51:59 -0300

    pass props to components

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
-function GoalItem () {
+function GoalItem ({text}) {
     return (
         <View style={styles.goalItem}>
-            <Text style={styles.goalText}>{itemData.item.text}</Text>
+            <Text style={styles.goalText}>{text}</Text>
         </View>
     )
 }
