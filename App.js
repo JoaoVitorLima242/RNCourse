dc258c69d57bfafb39595def3912693a1a0a1338
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 14:20:14 -0300

    seeing flex in mobile

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!'/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder='Your course goal!'/>
         <Button title='Add goals'/>
       </View>
       <View>
@@ -18,5 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  TextInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
   }
 });
