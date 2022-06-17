9c77666d72d3ec6425f7e3d3cbcb9c4579c041d7
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-16 22:35:15 -0300

    add some styles

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,7 +25,14 @@ function GoalInput ({onAddGoal, visible}) {
                     onChangeText={goalInputHandler}
                     value={enteredGoalText}
                 />
-                <Button title='Add goals' onPress={addGoalHandler}/>
+                <View style={styles.buttonContainer}>
+                    <View>
+                        <Button style={styles.button} title='Add goals' onPress={addGoalHandler}/>
+                    </View>
+                    <View>
+                        <Button style={styles.button} title='Cancel'/>
+                    </View>
+                </View>
             </View>
         </Modal>
     )
@@ -36,19 +43,27 @@ export default GoalInput
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 24,
         borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
+        borderBottomColor: '#ccc',
+        padding: 16
     },
     
     TextInput: {
         borderWidth: 1,
         borderColor: '#ccc',
-        width: '70%',
-        marginRight: 8,
+        width: '100%',
         padding: 8
     },
+
+    buttonContainer: {
+        marginTop: 10,
+        flexDirection: 'row'
+    },
+    button: {
+        width: '40%',
+        marginHorizontal: 8
+    }
 })
